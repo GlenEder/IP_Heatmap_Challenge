2f8bd464323d503211ac39c192b7c459f0b77f23
gleneder <geder@purdue.edu>
2021-01-26 20:58:37 -0500

    Adds start of server call getCords

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
+//set to false to disable server logging to console
+const serverLogs = true
 
 //csv-parse
 const csv = require('csv-parse')
 const fs = require('fs')
-const results = []
+const cords = []
 
 //read file
 fs.createReadStream('../GeoLite2-City-CSV_20190618/GeoLite2-City-Blocks-IPv4.csv')
-    .pipe(csv({})).on('data', data => results.push(data))
+    .pipe(csv({})).on('data', data => {
+        //only save the latitude and longitude values
+        let cord = {
+            lat: data[7],
+            long: data[8]
+        }
+
+        //save cords to results
+        cords.push(cord)
+    })
     .on('end', () => {
-        console.log("GeoLite Data Loaded")
+        //sort by lat for easier lookup
+        cords.sort( (a,b) => {return a.lat > b.lat ? 1 : -1} )
+
+        if(serverLogs) console.log("GeoLite Data Loaded")
     })
 
 
@@ -18,6 +32,7 @@ let app = express()
 
 //Landing page req
 app.get('/', (req, res) => {
+    if(serverLogs) console.log("Sending index.html to client...")
     res.sendFile('index.html', {root: '../client'})
 })
 
@@ -29,6 +44,32 @@ app.get('/main.css', (req, res) => {
 //Client script file
 app.get('/client.js', (req, res) => {
     res.sendFile('client.js', {root: '../client'})
+})
+
+
+/*==Never call this again, will crash browsers==*/
+app.get('/allCords', (req, res) => {
+    if(serverLogs) console.log("Sending all coordinates loaded...")
+
+    //null check on cords
+    if(cords === undefined || cords === null) res.send([])
+    else res.send(cords)
+})
+
+
+//Call to get cords in range specified by req.body
+//body { topLeft, bottomRight }
+app.post('/getCords', (req, res) => {
+
+    //Access values user sent
+    let top = req.body.topLeft[1]
+    let left = req.body.topLeft[0]
+    let bottom = req.body.bottomRight[1]
+    let right = req.body.bottomRight[0]
+
+    if(serverLogs) console.log("Finding cords within %d,%d -- %d,%d", left, top, right, bottom)
+
+    res.send([])
 })
 
 
